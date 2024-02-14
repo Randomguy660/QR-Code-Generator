4511807e4e855c9a0aed6d92c33de55cc0c47d16
Lewis Stopford <l.stopford@icloud.com>
2024-02-14 20:28:48 +0000

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import QRCode from "react-qr-code";
+import { useRef, useState } from 'react';
+
+function App() {
+  const input = useRef<HTMLInputElement>(null);
+  const [value, setValue] = useState<string>('');
+  const changeQRCode = () => {
+    if (input.current?.value == null) {
+      setValue('');
+    }
+    else{
+      setValue(input.current.value);
+    }
+  }
+
+  return (
+    <>
+      <div>
+        <QRCode id="QRCode" value={value} style={{ width: '500px', height: '500px'}}/>
+      </div>
+      <div className="container">
+        <input type="text" ref={input} onChange={changeQRCode} placeholder="Enter here..."/>
+        <label htmlFor="text">Enter text or URL</label>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
+
+
+
